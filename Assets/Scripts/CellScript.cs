5d5e40a10888dce4117b4024cb2d0d1ea093a047
Artem-Zhilinskiy <Arterm313132@gmail.com>
2022-12-10 14:35:07 +0300

    10.12.2022
    Запутался и решил начать заново

--- Assets/Scripts/CellScript.cs
+++ Assets/Scripts/CellScript.cs
@@ -8,5 +8,23 @@ namespace TowerDefense
     public class CellScript : MonoBehaviour
     {
         public bool _isGround;
+        public bool _hasTower = false;
+
+        public Color _baseColor;
+        public Color _currentColor;
+
+        private void OnMouseEnter()
+        {
+            if (!_isGround)
+            {
+                Debug.Log("Is something going on?");
+                GetComponent<SpriteRenderer>().color = _currentColor;
+            }
+        }
+
+        private void OnMouseExit()
+        {
+            GetComponent<SpriteRenderer>().color = _baseColor;
+        }
     }
 }
---
+++ Assets/Scripts/MoneyManagerScript.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace TowerDefense
+{
+    public class MoneyManagerScript : MonoBehaviour
+    {
+        public static MoneyManagerScript Instance;
+        public Text _moneyText;
+        public int _gameMoney;
+
+        private void Awake()
+        {
+            Instance = this;
+        }
+
+        private void Update()
+        {
+            _moneyText.text = _gameMoney.ToString();
+        }
+
+    }
+}
